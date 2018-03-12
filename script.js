087b6e5c9dd97f7264bfc6dd1dedaab4f2fcc24e
Yaroslav Filipenko <f1l1penko@ukr.net>
2018-03-12 18:25:25 +0300

    first

---
+++ script.js
@@ -0,0 +1,63 @@
+//           N1
+
+// function reverse(string) {
+// 	var newStr = '';
+// 	for (var i = string.length - 1; i >= 0; i--) {
+// 		newStr = newStr + string[i];
+// 	}
+// 	return newStr;
+// }
+
+// alert(reverse('Привет мир'));
+
+// ---------------------------------------------------
+
+// function rev(str) {
+//     return str.split("").reverse().join("");
+// }
+
+// alert(rev("Привет мир!"));
+
+
+//           N2
+
+// function camelize(str) {
+  
+// var arr = str.split('');
+
+// arr[0] = arr[0].toUpperCase();
+// arr[arr.length - 1] = arr[arr.length - 1].toUpperCase();
+
+// for(i=0; i<arr.length; i++) {
+//   if(arr[i]===' ') {(arr[i-1] = arr[i-1].toUpperCase()) && (arr[i+1] = arr[i+1].toUpperCase())};
+// }
+
+// return arr.join('');
+ 
+// }
+
+
+// alert(camelize('aldkg adkgjmla alwkmgfla asdlknm'))
+
+
+//             N3
+
+
+// function isWeekEnd(date) {
+// 	if (date.getDay == 0 || date.getDay == 6) {
+// 		return "Выходной" 
+// 	} else return "Работаем дальше"
+// }
+
+// 	var date = new Date(2018, 3, 4);
+// 	console.log(isWeekEnd(date));
+
+
+
+//             N4
+
+var code = prompt('Введите код');
+function runJS(string){
+	
+
+}
